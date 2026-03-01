7af09073e75dec9fea7a115f5bc605fd134d1ea2
BorisZhang12 <zhangboris12@gmail.com>
2026-02-28 18:39:03 -0800

    Refactor Leaderboard into single-view dropdown toggle

--- src/leaderboard/LeaderboardContainer.jsx
+++ src/leaderboard/LeaderboardContainer.jsx
@@ -1,13 +1,32 @@
+import React, { useState } from 'react';
 import FriendsLeaderboard from './FriendsLeaderboard';
 import GlobalLeaderboard from './GlobalLeaderboard';
 import './Leaderboard.css';
 
 export default function LeaderboardContainer({ userElo, userName }) {
+    const [selectedLeaderboard, setSelectedLeaderboard] = useState('global');
+
     return (
         <div className="leaderboard-container">
-            <div className="leaderboard-grid">
-                <GlobalLeaderboard userElo={userElo} userName={userName} />
-                <FriendsLeaderboard userElo={userElo} userName={userName} />
+            <div className="leaderboard-selector-container">
+                <label htmlFor="leaderboard-select" className="selector-label">View Rankings:</label>
+                <select
+                    id="leaderboard-select"
+                    className="leaderboard-select"
+                    value={selectedLeaderboard}
+                    onChange={(e) => setSelectedLeaderboard(e.target.value)}
+                >
+                    <option value="global">Global Leaderboard</option>
+                    <option value="friends">Friends Leaderboard</option>
+                </select>
+            </div>
+
+            <div className="leaderboard-content">
+                {selectedLeaderboard === 'global' ? (
+                    <GlobalLeaderboard userElo={userElo} userName={userName} />
+                ) : (
+                    <FriendsLeaderboard userElo={userElo} userName={userName} />
+                )}
             </div>
         </div>
     );
