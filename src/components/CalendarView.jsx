218304e02e27eede7fdc50c2d59c13d43cb967a4
Andrew Yan <andrewyan0226@gmail.com>
2026-02-28 16:31:25 -0800

    Fixed calendar tabs

--- src/components/CalendarView.jsx
+++ src/components/CalendarView.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
+import React, { useState } from 'react'
+import { Calendar, dateFnsLocalizer, Views } from 'react-big-calendar'
 import format from 'date-fns/format'
 import parse from 'date-fns/parse'
 import startOfWeek from 'date-fns/startOfWeek'
@@ -21,6 +21,9 @@ const localizer = dateFnsLocalizer({
 })
 
 export default function CalendarView({ events }) {
+    const [view, setView] = useState(Views.MONTH)
+    const [date, setDate] = useState(new Date())
+
     return (
         <div className="calendar-container">
             <Calendar
@@ -30,6 +33,10 @@ export default function CalendarView({ events }) {
                 endAccessor="end"
                 style={{ height: 'calc(100vh - 120px)' }}
                 className="premium-calendar"
+                view={view}
+                onView={setView}
+                date={date}
+                onNavigate={setDate}
             />
         </div>
     )
